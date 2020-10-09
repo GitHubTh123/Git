a42c3ddb3f9906ada2d5bf4b97f826a1ea6adf08
th <firstforgithub@163.com>
2020-10-09 15:27:44 +0800

    原始结构

---
+++ src/main/java/com/atguigu/Demo.java
@@ -0,0 +1,14 @@
+package com.atguigu;
+
+/**
+ * Created by with Intellij IDEA.
+ *
+ * @Auther:唐鸿
+ * @Date: 2020/10/9 15:25
+ * @Description:
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+    }
+}
